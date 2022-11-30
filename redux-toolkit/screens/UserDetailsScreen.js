6853b25be56cd7f036c9ea8835f4501168118b94
Asim Riaz <asim.riaz@live.com>
2022-11-30 11:48:21 +0500

    Updates

--- redux-toolkit/screens/UserDetailsScreen.js
+++ redux-toolkit/screens/UserDetailsScreen.js
@@ -1,23 +1,54 @@
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, StyleSheet, Image, ScrollView } from "react-native";
+import { Stack, TextInput, IconButton, BackdropSubheader } from "@react-native-material/core";
+import Icon from "@expo/vector-icons/MaterialCommunityIcons";
+import React from "react";
 
 const HomeScreen = ({ route, navigation }) => {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details</Text>
-      <Text>{JSON.stringify(route.params)}</Text>
-
-    </View>
-  )
-}
-
+    const { _id, name, dob, email, image, nat } = route.params;
+    return (
+        // <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        //     <Text>Details</Text>
+        <ScrollView>
+            <Stack spacing={2} style={{ margin: 16 }}>
+                <Text>{JSON.stringify(route.params)}</Text>
+                {/* <TextInput label="Label" leading={(props) => <Icon name="account" {...props} />} />
+            <TextInput
+                label="Label"
+                variant="outlined"
+                trailing={(props) => <IconButton icon={(props) => <Icon name="eye" {...props} />} {...props} />}
+            /> */}
+                <Image source={{ uri: image }} style={{ height: 200, width: 200, alignSelf: "center" }} />
+                <BackdropSubheader style={styles.header} title="Name" />
+                <TextInput style={styles.input} label="Title" variant="standard" value={name.title} />
+                <TextInput style={styles.input} label="First Name" variant="standard" value={name.first} />
+                <TextInput style={styles.input} label="Last Name" variant="standard" value={name.last} />
+                <Text style={styles.spacer} />
+                <BackdropSubheader style={styles.header} title="Date of Birth" />
+                <TextInput style={styles.input} label="Date" variant="standard" value={dob.date} />
+                <TextInput style={styles.input} label="Age" variant="standard" value={dob.age} />
+                <TextInput style={styles.input} label="Email" variant="standard" value={email} />
+            </Stack>
+        </ScrollView>
+        // </View>
+    );
+};
 
 const styles = StyleSheet.create({
-	link: {
-		color: "blue",
-		textDecorationLine: "underline",
-		fontSize: 20,
-	},
+    link: {
+        color: "blue",
+        textDecorationLine: "underline",
+        fontSize: 20,
+    },
+    input: {
+        marginVertical: 15,
+    },
+    header: {
+        color: "blue",
+        fontWeight: "bold",
+    },
+    spacer: {
+        marginVertical: 20,
+    },
 });
 
-export default HomeScreen
+export default HomeScreen;
